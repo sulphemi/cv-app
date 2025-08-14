6d2a118644ec2de2d35299b586679388c40e19dd
Jing Qian <jqian2272@gmail.com>
2025-08-14 03:06:04 -0400

    wip working

--- App.tsx
+++ App.tsx
@@ -3,9 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import EducationForm from './components/EducationForm.tsx'
 import ExperienceForm from './components/ExperienceForm.tsx'
+import ResumePage from './components/ResumePage.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ formData, setFormData ] = useState({
+    "name": "Reimu Harukei",
+    "email": "something@example.com",
+    "phone": "(111) 222-333",
+  })
+
+  const updateData = (key, value) => {
+    setFormData({
+      ...formData,
+      [ key ]: value,
+    })
+
+    console.log(formData)
+  }
 
   return (
     <>
@@ -13,13 +27,16 @@ function App() {
         <form>
           <h2>Personal Info</h2>
           <p>Name:</p>
-          <input type="text" />
+          <input type="text" id="nameInput"
+            onChange={(e) => updateData("name", e.target.value)}
+          />
+          <p>Your name is {formData["name"]}</p>
 
           <p>Email:</p>
-          <input type="text" />
+          <input type="text" id="emailInput"/>
 
           <p>Phone Number:</p>
-          <input type="text" />
+          <input type="text" id="phoneInput"/>
           
           <div>
             <button>Next</button>
@@ -48,6 +65,8 @@ function App() {
             <button>Submit</button>
           </div>
         </form>
+
+        <ResumePage id="printable-resume" formData={formData}/>
       </div>
     </>
   )
---
+++ components/ResumePage.tsx
@@ -0,0 +1,18 @@
+function ResumePage({ formData }) {
+  return (
+    <div>
+      <h1>{formData["name"]}</h1>
+      <p>{formData["email"]}</p>
+      <p>{formData["phone"]}</p>
+      <hr />
+      <h2>Education</h2>
+      <ul>
+        <li>thingy thing</li>
+      </ul>
+      <hr />
+      <h2></h2>
+    </div>
+  )
+}
+
+export default ResumePage
